21a364b3f964421d857e8118009f89af05f55f8e
dmartin5150 <dmartin5150@icloud.com>
2025-05-05 23:08:51 -0500

    updated origins

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ load_dotenv()
 
 app = FastAPI()
 
-# Allow CORS from your Vercel frontend
-origins = ["https://surgical-analytics.vercel.app"]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["https://surgical-analytics.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
